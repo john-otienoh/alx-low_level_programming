20ea88980bbffc098a3881fc4694ca7b0270f006
john-otienoh <johnteclaire@gmail.com>
2023-06-22 23:14:04 -0700

    ./6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,3 +1,20 @@
 #include "main.h"
+
 /**
- *
+ * _abs -  function that prints an absolute  number.
+ * @x: stores the number
+ * @absolute - st0res absolute  integer
+ * Return: Always 0(success)
+ */
+int _abs(int x)
+{
+	int absolute;
+
+	if (x < 0)
+	{
+		absolute = x * -1;
+		return (absolute);
+	}
+	else
+		return (x);
+}
