2a8d44c780aa9732c8ef3ec43b0f04cd73fb8ec3
john-otienoh <johnteclaire@gmail.com>
2023-07-14 03:04:27 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - program that prints sum of even fibonacci
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int i, c;
+	int sum = 0;
+
+	for (i = 0; i <= 29; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c % 2 == 0)
+		{
+		    sum = sum + c;
+		}
+	}
+	printf("%d", sum + 2);
+	putchar('\n');
+	return (0);
+}
