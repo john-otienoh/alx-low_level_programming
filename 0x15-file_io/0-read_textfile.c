b10faf5bf88d86f5b70f4e8fe0c9426a9de7882d
john-otienoh <johnteclaire@gmail.com>
2023-11-16 12:12:44 +0300

    File input & Output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,39 @@
 #include "main.h"
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
 /**
- * read_textfile -  function that reads a text file and prints 
- * it to the POSIX standard output.
- * @letters: number of letters it should read and print
- * @filename: The name of file to be executed
- * Return: actual number of letters it could read and print or 0.
- */
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: name of the file that we will open
+* @letters: number of letters it should read and print
+* Return: the actual number of letters that we could read and print, 0 if fails
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fD;
-	ssize_t _read, _write;
-	char *buffer;
+	char *mybuf = malloc(sizeof(char) * letters);
+	int fd;
+	ssize_t rl, rw;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
-	fD = open(filename, O_RDONLY);
-	if (fD == -1)
-	{
-		return (0);
-	}
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		return (0);
-	}
-	_read = read(fD, buffer, letters);
-	if (_read == -1)
-	{
-		free(buffer);
-		close(fD);
-		return (0);
-	}
-	if (_write == -1)
-	{
-		free(buffer);
-		close(fD);
-		return (0);
-	}
-	close(fD);
-	return (_read);
-}
 
+	fd = open(filename, O_RDWR);
+
+	if (fd == -1)
+		return (0);
+
+	rl = read(fd, mybuf, letters);
+	if (rl < 0)
+		return (0);
+
+	rw = write(STDOUT_FILENO, mybuf, rl);
+	if (rw < 0)
+		return (0);
+
+	free(mybuf);
+	close(fd);
+	return (rw);
+}
