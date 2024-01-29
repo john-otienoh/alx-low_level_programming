17a063287103260efac470abe18855ae76932486
john-otienoh <johnteclaire@gmail.com>
2024-01-29 22:34:00 +0300

    0x18-dynamic libraries

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+/*    char *word = "My Dyn Lib";
+    int len = _strlen(word);
+    printf("%d\n", len);*/
+    return (EXIT_SUCCESS);
+}
