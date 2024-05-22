45a2318b71eccbdd09a18354b83e76d34c1e0d1e
john-otienoh <johnteclaire@gmail.com>
2024-05-22 11:46:27 -0700

    binary serch

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,42 +1,60 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * binary_search - function that searches for a value in an array of integers
- * using the Binary search algorithm.
- * @array: is a pointer to the first element of the array to search in
- * @size: is the number of elements in array.
- * @value: is the value to search for.
- * Return: the first index where value is located or -1.
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int recursive_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int binary_search(int *array, size_t size, int value)
 {
-	unsigned long int flag = 0, mid, low = 0, high = size - 1;
+	int index;
 
-	if (array == NULL)
-	{
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
 		return (-1);
-	}
-	while (low <= high)
-	{
-		mid = (int) floor((low + high) / 2);
-	
-		if (array[mid] == value)
-		{
-			flag = 1;
-			break;
-		}
-		else if (array[mid] < value)
-		{
-			high = mid - 1;
-		}
-		else
-		{
-			low = mid + 1;
-		}
-	}
-	if (flag == 1)
-		return (mid);
-	else
-		return (-1);
+
+	return (index);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,8 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 #include <stdio.h>
-#include <math.h>
 
+int recursive_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
